9d63630e315dc2e1c7f8c3df9f066a71f09f951c
Luc Mcgrady <lucmcgrady@gmail.com>
2023-05-12 22:02:12 +0100

    Added: Earliest latest

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from aqt.gui_hooks import deck_browser_will_show_options_menu
-# from anki.decks import DeckManager
+from anki.decks import DeckManager
+from array import array
 # from anki.exporting import AnkiExporter
 from aqt import mw, QMenu
 
@@ -19,9 +20,12 @@ def getDeck(did):
     # exporter.did = did
     card_ids = mw.col.decks.cids(did, children=True)
     #for card in card_ids:
-    card = card_ids[0]
-    queue = mw.col.card_stats_data(card).revlog
 
-    print(f"{did=} {queue=}")
+    day = 60 * 60 * 24
+    revlogs = [mw.col.card_stats_data(card).revlog for card in card_ids]
+    earliest = min(revlogs, key=lambda a:a[-1].time if len(a) > 0 else 2e50)[-1].time // day # Gets the earliest day reviewed on 
+    latest = max(revlogs, key=lambda a:a[0].time if len(a) > 0 else -1)[0].time // day # Gets the latest day reviewed on
+
+    print(f"{earliest=} {latest=}")
 
 action(getDeck, "Create timelapse")
