57d6cbbe428fdfdb90a095a61b69f372e0f4d88a
Luc Mcgrady <lucmcgrady@gmail.com>
2023-05-12 23:32:16 +0100

    Sorta works

--- __init__.py
+++ __init__.py
@@ -21,11 +21,39 @@ def getDeck(did):
     card_ids = mw.col.decks.cids(did, children=True)
     #for card in card_ids:
 
-    day = 60 * 60 * 24
+    day_seconds = 60 * 60 * 24
     revlogs = [mw.col.card_stats_data(card).revlog for card in card_ids]
-    earliest = min(revlogs, key=lambda a:a[-1].time if len(a) > 0 else 2e50)[-1].time // day # Gets the earliest day reviewed on 
-    latest = max(revlogs, key=lambda a:a[0].time if len(a) > 0 else -1)[0].time // day # Gets the latest day reviewed on
+    revlogs = [revlog for revlog in revlogs if len(revlog) > 0]
 
-    print(f"{earliest=} {latest=}")
+    earliest = min(revlogs, key=lambda a:a[-1].time if len(a) > 0 else 2e50)[-1].time // day_seconds # Gets the earliest day reviewed on 
+    latest = max(revlogs, key=lambda a:a[0].time if len(a) > 0 else -1)[0].time // day_seconds # Gets the latest day reviewed on
+
+    empty_day = [0, 0, 0, 0]
+
+    days = [empty_day] * (latest - earliest) # Create an empty array
+    for revlog in revlogs:
+        review = -1 # 1 indexed because we're working backwards
+
+        day = revlog[review].time // day_seconds
+        print(revlog)
+
+        while day < latest - 1:
+
+            while revlog[review-1].time // day_seconds <= day:
+                #print(f"{review=}, {revlog[review].time // day_seconds=}, {day=}")
+                review -= 1
+            
+            current = revlog[review].review_kind
+            next_time = revlog[review-1].time // day_seconds if review > -len(revlog) + 2 else latest - 1
+            print(f"{current=} {next_time=} {review=}>{-len(revlog)=}")
+
+            while day < next_time:
+                #print(f"{day - earliest=} {len(days)=} {next_time - earliest=}")
+                days[day - earliest][current] += 1
+                day += 1
+
+
+
+    print(f"{days=}")
 
 action(getDeck, "Create timelapse")
