3beb2aef7c736c200c70b64c8c219ec94a4cce01
Luc Mcgrady <lucmcgrady@gmail.com>
2023-05-14 15:38:46 +0100

    Added: Day class

--- interval.py
+++ interval.py
@@ -12,7 +12,30 @@ class IdHashedList(list):
     def __hash__(self) -> int:
         return id(self)
 
-def get_days_intervals(did):
+class Day:
+    new: int
+    intervals: IdHashedList[int]
+
+    def __init__(self):
+        self.intervals = IdHashedList([0] * 500)
+        self.new = 0
+
+    @property
+    def learning(self):
+        return self.intervals[0]
+    
+    @property
+    def young(self):
+        return sum(self.intervals[1:21])
+
+    @property
+    def mature(self):
+        return sum(self.intervals[21:])
+    
+    def __iter__(self):
+        return self.intervals
+
+def get_days(did):
     card_ids = mw.col.decks.cids(did, children=True)
     #for card in card_ids:
 
@@ -23,9 +46,7 @@ def get_days_intervals(did):
     earliest = min(cards, key=lambda a:a.first_review).first_review // day_seconds # Gets the earliest day reviewed on 
     latest = max(cards, key=lambda a:a.latest_review).latest_review // day_seconds # Gets the latest day reviewed on
 
-    empty_day = [0] * 500
-
-    intervals = [IdHashedList(empty_day.copy()) for _ in range(earliest, latest)] # Create an empty array
+    days = [Day() for _ in range(earliest, latest)] # Create an empty array
     for card in cards:
         # mw.col.get_card(card.id)
 
@@ -33,18 +54,26 @@ def get_days_intervals(did):
         last = copy(revlog[0])
         last.time = latest * day_seconds
         ranges = zip(revlog, [*revlog[1:], last])
+
+        added = card.added // day_seconds
+        added = added if added > earliest else earliest 
         
+        for i in range(added, revlog[0].time // day_seconds):
+            print(f"{i} - {earliest} = {i-earliest}")
+            day = days[i - earliest]
+            day.new += 1
+
         #print()
         for current, next in ranges:
             #print(f"{current.time // day_seconds=}, {next.time // day_seconds=}")
             for i in range(current.time // day_seconds, next.time // day_seconds):
-                day = intervals[i - earliest]
+                day = days[i - earliest]
                 index = current.interval // day_seconds
 
                 #day_current = day.get(index, 0) + 1
-                day[index] += 1
+                day.intervals[index] += 1
     
-    return intervals
+    return days
 
 def interval_timelapse(
         did,
@@ -54,7 +83,7 @@ def interval_timelapse(
     ):
     deck = DeckManager(mw.col).get(did)
 
-    intervals = get_days_intervals(did)
+    days = get_days(did)
 
     plt.style.use("seaborn")
     fig = Figure()
@@ -62,7 +91,7 @@ def interval_timelapse(
     axes.set_title(f"{deck['name']} Intervals")
     bars = axes.bar(range(0,500),[0] * 500)
 
-    frames = (len(intervals) - 1) * frames_per_day
+    frames = (len(days) - 1) * frames_per_day
 
     def lerp(a: int, b: int, t: float):
         return a + (b - a) * t
@@ -89,16 +118,16 @@ def interval_timelapse(
         day_index = frame // frames_per_day
         sub_frame = (frame % frames_per_day) / frames_per_day
 
-        day: IdHashedList[int] = intervals[day_index]
-        next_day = intervals[day_index+1]
+        intervals: IdHashedList[int] = days[day_index].intervals
+        next_intervals = days[day_index+1].intervals
 
-        axes.set_xlim(0, lerp(last_day(day), last_day(next_day), sub_frame))
-        axes.set_ylim(0, lerp(memo_max(day), memo_max(next_day), sub_frame))
-        axes.set_ylabel(f"Total cards: {sum(day)}") 
-        axes.set_xlabel(f"Average interval: {mean(day):.2f} Days") 
+        axes.set_xlim(0, lerp(last_day(intervals), last_day(next_intervals), sub_frame))
+        axes.set_ylim(0, lerp(memo_max(intervals), memo_max(next_intervals), sub_frame))
+        axes.set_ylabel(f"Total cards: {sum(intervals)}") 
+        axes.set_xlabel(f"Average interval: {mean(intervals):.2f} Days") 
 
         for i, b in enumerate(bars):
-            b.set_height(lerp(day[i], next_day[i], sub_frame))
+            b.set_height(lerp(intervals[i], next_intervals[i], sub_frame))
         
         print(f"{frame=}/{frames}")
 
